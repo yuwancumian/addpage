3409996d70022bb591eb9fab788368125bbe7afd
yuwancumian <easier2005@163.com>
2015-10-22 17:28:09 +0800

    require cheerio

--- bin/addpage.js
+++ bin/addpage.js
@@ -3,14 +3,15 @@ require('shelljs/global');
 
 var fs = require('fs');
 var path = require('path');
+var cheerio = require('cheerio');
 
-cd('app'); 
 var files = fs.readdirSync('.');
 var html = files.filter(function(item){
 	return path.extname(item) === '.html';
 });
 
 var all = fs.readFileSync(html[1],'utf-8');
-console.log(all);
 
-console.log("123r");
+var  $ = cheerio.load(all);
+console.log($(all).find('head').html());
+console.log($('script').html());
